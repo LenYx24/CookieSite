38ab5dd61c46a0ef87ab872077dafb8ce1b9e7f8
LenYx24 <fokilenard@gmail.com>
2022-05-18 21:53:06 +0200

    fixed navbar links

--- src/Navbar_components/NavLinks.js
+++ src/Navbar_components/NavLinks.js
@@ -2,7 +2,24 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const NavLinks = ({ menuVisible }) => {
-  const NavLinkNames = ["", "shop", "about me", "contact"];
+  const NavLinkNames = [
+    {
+      link: "",
+      name: "home",
+    },
+    {
+      link: "shop",
+      name: "shop",
+    },
+    {
+      link: "aboutme",
+      name: "about me",
+    },
+    {
+      link: "contact",
+      name: "contact",
+    },
+  ];
   return (
     <div
       className={`flex lg:flex-row flex-col text-md lg:text-sm items-center lg:inline-flex order-2 lg:order-1 ${
@@ -12,11 +29,11 @@ const NavLinks = ({ menuVisible }) => {
       {NavLinkNames.map((item, index) => {
         return (
           <Link
-            to={item.replace(" ", "")}
+            to={item.link}
             className={`px-1 lg:px-4 uppercase font-light hover:underline transition-opacity`}
             key={index}
           >
-            {item}
+            {item.name}
           </Link>
         );
       })}
